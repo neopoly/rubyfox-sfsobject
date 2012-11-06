1a5c424138de6b1a236dfe3f67ba178b2b418c1c
Peter Suschlik <ps@neopoly.de>
2012-11-06 15:47:53 +0100

    Handle schema for core ext.

--- lib/rubyfox/sfsobject/extend.rb
+++ lib/rubyfox/sfsobject/extend.rb
@@ -1,15 +1,24 @@
 # Extends core object with some conversion methods.
 
 require 'rubyfox/sfsobject/bulk'
+require 'rubyfox/sfsobject/schema'
 
 class Hash
-  def to_sfs
-    Rubyfox::SFSObject::Bulk.to_sfs(self)
+  def to_sfs(schema=:none)
+    if schema == :none
+      Rubyfox::SFSObject::Bulk.to_sfs(self)
+    else
+      Rubyfox::SFSObject::Schema.to_sfs(schema, self)
+    end
   end
 end
 
 class Rubyfox::SFSObject::Java::SFSObject
-  def to_hash
-    Rubyfox::SFSObject::Bulk.to_hash(self)
+  def to_hash(schema=:none)
+    if schema == :none
+      Rubyfox::SFSObject::Bulk.to_hash(self)
+    else
+      Rubyfox::SFSObject::Schema.to_hash(schema, self)
+    end
   end
 end
--- test/rubyfox/sfsobject/extend_test.rb
+++ test/rubyfox/sfsobject/extend_test.rb
@@ -2,8 +2,26 @@ require 'helper'
 require 'rubyfox/sfsobject/extend'
 
 class RubyfoxSFSObjectExtendTest < RubyfoxCase
-  test "bulk methods" do
-    hash = { :key => "string"}
-    assert_equal hash, hash.to_sfs.to_hash
+  context "bulk" do
+    test "methods" do
+      hash = { :key => "value"}
+      assert_equal hash, hash.to_sfs.to_hash
+    end
+  end
+
+  context "schema" do
+    test "methods" do
+      schema = { :key => String }
+      hash = { :key => "value" }
+      assert_equal hash, hash.to_sfs(schema).to_hash(schema)
+    end
+
+    test "fail w/o valid schema" do
+      schema = nil
+      hash = { :key => "value" }
+      assert_raises NoMethodError, :message => /each/ do
+        assert_equal hash, hash.to_sfs(schema).to_hash(schema)
+      end
+    end
   end
 end
